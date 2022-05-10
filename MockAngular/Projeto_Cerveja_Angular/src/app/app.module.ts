1fc03be8249f9e7e07eabb27104f20b76211efbf
Kauã Rabelo <kauarabelo1@gmail.com>
2022-05-10 17:17:08 -0300

    v2.1
    Adicionando menu ao angular

---
+++ MockAngular/Projeto_Cerveja_Angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardapioComponent } from './cardapio/cardapio.component';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardapioComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ MockAngular/Projeto_Cerveja_Angular/src/app/menu/menu.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+// import {PageCardapio} from '../cardapio'
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+  
+
+}
