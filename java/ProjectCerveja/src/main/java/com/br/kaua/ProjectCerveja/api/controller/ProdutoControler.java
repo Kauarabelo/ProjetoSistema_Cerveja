4ee477986758978ef6091f0b47d39871508c4d82
Kauã Rabelo <kauarabelo1@gmail.com>
2022-08-15 17:16:11 -0300

    v5.38

--- java/ProjectCerveja/src/main/java/com/br/kaua/ProjectCerveja/api/controller/ProdutoControler.java
+++ java/ProjectCerveja/src/main/java/com/br/kaua/ProjectCerveja/api/controller/ProdutoControler.java
@@ -5,42 +5,38 @@
  */
 package com.br.kaua.ProjectCerveja.api.controller;
 
-import com.br.kaua.ProjectCerveja.domain.model.Pedido;
-import com.br.kaua.ProjectCerveja.domain.repository.PedidoRepository;
-import com.br.kaua.ProjectCerveja.domain.service.PedidoService;
+import com.br.kaua.ProjectCerveja.domain.model.Produto;
+import com.br.kaua.ProjectCerveja.domain.repository.ProdutoRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 
 /**
  *
  * @author devsys-b
  */
 @RestController
-public class PedidoControler {
-
+@RequestMapping("/produto")
+public class ProdutoControler {
+    
     @Autowired
-    private PedidoRepository pedidoRepository;
+    private ProdutoRepository produtoRepository;
 
-    @Autowired
-    private PedidoService pedidoService;
+    @GetMapping
+    public List<Produto> listarProduto() {
+        return produtoRepository.findAll();
+    }
 
-    @GetMapping("/pedido")
-    public List<Pedido> listar() {
-        return pedidoRepository.findAll();
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto adionarProduto(@RequestBody Produto produto) {
+        return produtoRepository.save(produto);
     }
     
-    @PostMapping("/pedido")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Pedido criar(@RequestBody Pedido pedido){
-        return pedidoService.criar(pedido);
-    }
-
 }
--- java/ProjectCerveja/src/main/java/com/br/kaua/ProjectCerveja/domain/repository/ProdutoRepository.java
+++ java/ProjectCerveja/src/main/java/com/br/kaua/ProjectCerveja/domain/repository/ProdutoRepository.java
@@ -5,15 +5,15 @@
  */
 package com.br.kaua.ProjectCerveja.domain.repository;
 
-import com.br.kaua.ProjectCerveja.domain.model.Pedido;
+import com.br.kaua.ProjectCerveja.domain.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 /**
  *
  * @author devsys-b
  */
 @Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
     
 }
-
